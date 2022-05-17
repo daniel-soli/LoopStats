93d62a605f2e43209c2bd2893f52b2ef3d21f942
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-05-17 03:56:22 +0200

    Changed from string to int

--- src/LoopStats/Models/Entities/LoopringStatsEntity.cs
+++ src/LoopStats/Models/Entities/LoopringStatsEntity.cs
@@ -7,14 +7,14 @@ namespace LoopStats.Models.Entities;
 
 public class LoopringStatsEntity : TableEntity, IMapFrom<BlockStatsDto>
 {
-    public string blockCount { get; set; }
-    public string transactionCount { get; set; }
-    public string transferCount { get; set; }
-    public string transferNFTCount { get; set; }
-    public string tradeNFTCount { get; set; }
-    public string nftMintCount { get; set; }
-    public string userCount { get; set; }
-    public string nftCount { get; set; }
+    public long blockCount { get; set; }
+    public long transactionCount { get; set; }
+    public long transferCount { get; set; }
+    public long transferNFTCount { get; set; }
+    public long tradeNFTCount { get; set; }
+    public long nftMintCount { get; set; }
+    public long userCount { get; set; }
+    public long nftCount { get; set; }
 
     public void Mapping(Profile profile)
     {
